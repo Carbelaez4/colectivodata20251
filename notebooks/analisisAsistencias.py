9f57d0fec6d25b4e8bbca2f668b5010abe813b15
carbelaez4 <carbelaez4@gmail.com>
2025-04-23 10:26:36 -0500

    Se crea los archivos en notebook y el archivo gitignore y paquetes

---
+++ notebooks/analisisAsistencias.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+#Extraer la informacion
+dataFrameAsistencia=pd.read_csv("./data/asistencia_estudiantes_completo.csv")
+
+
+
+#Extraer datos basicos de la data ingresada
+
+#print(dataFrameAsistencia.head(15))
+#print(dataFrameAsistencia.tail())
+#print(dataFrameAsistencia.info())
+#print(dataFrameAsistencia.describe())
+print(dataFrameAsistencia['estrato'].value_counts().head(2))
---
+++ notebooks/analisisUsuarios.py
@@ -0,0 +1,3 @@
+import pandas as pd
+dataFrameUsuario=pd.read_excel("./data/usuarios_sistema_completo.xlsx")
+print(dataFrameUsuario)
